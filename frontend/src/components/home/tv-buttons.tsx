f434367cf8e8247c25878d6ea27b17299a876ef2
jerome <jerome.studypurpose@gmail.com>
2024-03-12 03:00:15 +0800

    changed button name

--- frontend/src/components/home/tv-buttons.tsx
+++ frontend/src/components/home/tv-buttons.tsx
@@ -3,18 +3,20 @@ export const TvButtonsComponent = ({ setMovie }: { setMovie: Function}) => {
 
     const [active, setActive] = useState<string>("popular");
     const buttons = ["popular", "airing_today", "top_rated"];
-
     const handleClick = (e: MouseEvent<HTMLElement>) => {
         const button = e.target as HTMLInputElement;
         setActive(button.value);
         setMovie(button);
     };
+    // Changed Buttons Name
     return(
         <>
             <div className="border border-3 rounded-pill">
                 {buttons.map((button) => {
                     return(
-                        <button style={{paddingInline: "10px"}} value={button} key={button} className={active === button? `${button}-radius tv-button-edit button-size`: "button-size tv-button-edit rounded-3"} onClick={handleClick}>{button}</button>
+                        <button value={button} key={button} className={active === button? `${button}-radius tv-button-edit button-size`: "button-size tv-button-edit rounded-3"} onClick={handleClick}>
+                            {button === "popular" ? "Popular" : button === "airing_today"? "Airing Today" : "Top Rated" }
+                        </button>
                     )
                 })}
             </div>
